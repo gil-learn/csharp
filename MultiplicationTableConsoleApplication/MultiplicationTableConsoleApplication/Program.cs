edac6fe9af5c3233c912214fb7bfb643c759367c
gil-learn <bombamela9@yahoo.com>
2015-12-10 17:15:36 +0200

    Multiplication table console app

---
+++ MultiplicationTableConsoleApplication/MultiplicationTableConsoleApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTableConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1; i<=10; i++)
+            {
+                for (int j=1; j<=10; j++)
+                {
+                    int Result = i * j;
+                    Console.Write(Result.ToString() + "\t");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
